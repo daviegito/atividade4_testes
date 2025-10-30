e90eb8b5dce617cc83f15e1f30881328ef81b3f5
daviegito <daviegitocoelho@gmail.com>
2025-10-30 17:53:29 -0300

    feat: adição do ciclo 2

--- src/ehExpressaoValida.py
+++ src/ehExpressaoValida.py
@@ -1,2 +1,10 @@
-def ehExpressaoValida (expressao):
-  return False
+def ehExpressaoValida(expressao):
+    # remoção de espaços da expressão
+    expr_sem_espaco = expressao.replace(" ", "")
+
+    # se após remover os espaços a string estiver vazia, tem que retornar falso
+    if expr_sem_espaco == "":
+        return False
+
+    # retorne falso pra qualquer outra expressão
+    return False
--- tests/test_ehExpressaoValida.py
+++ tests/test_ehExpressaoValida.py
@@ -1,4 +1,9 @@
 from src.ehExpressaoValida import ehExpressaoValida
 
+
 def test_deveRejeitarExpressaoVazia():
     assert ehExpressaoValida("") == False
+
+
+def test_deveRejeitarExpressaoApenasComEspacos():
+    assert ehExpressaoValida(" ") == False
